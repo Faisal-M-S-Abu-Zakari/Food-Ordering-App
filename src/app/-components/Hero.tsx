f3a5e6b5d6acd48607ff0053ca4c44b710d7a9c9
Faisal <zkryfysl@gmail.com>
2025-06-05 12:23:36 +0300

    i make _components that will include the component that will be used in the root page only , i made the Hero section and add some sharing style to gloabls.css file

---
+++ src/app/-components/Hero.tsx
@@ -0,0 +1,60 @@
+import Image from "next/image";
+import React from "react";
+import pizza from "../../../public/assets/images/pizza.png";
+import Link from "@/components/link";
+import { buttonVariants } from "@/components/ui/button";
+import { Routes } from "@/constants/enums";
+import { ArrowRightCircle } from "lucide-react";
+const Hero = () => {
+  return (
+    // i make this class = section-gap , it will be in the gloabls.css file =>read it
+    <section className="section-gap">
+      <div className="container grid grid-cols-1 md:grid-cols-2">
+        {/* the height of this div will control the height to the image div , so when i add a lot of text the image will take more height */}
+        <div className="md:py-12">
+          {/* it is to important to be h1 for SEO */}
+          <h1 className="text-4xl font-semibold">Slice into Hapiness</h1>
+          <p className="text-accent my-4">
+            Craving pizza? We've got you covered with fresh ingredients ,
+            endless flavors, and the fastest delivery. Your perfect slice is jut
+            a tap away!
+          </p>
+          <div className="flex items-center gap-4">
+            <Link
+              href={`/${Routes.MENU}`}
+              className={`${buttonVariants({
+                size: "lg",
+              })} space-x-2 !px-4 !rounded-full uppercase`}
+            >
+              ORDER NOW
+              <ArrowRightCircle className={`!w-5 !h-5 `} />
+            </Link>
+            <Link
+              href={`/${Routes.ABOUT}`}
+              className="flex gap-2 items-center text-black hover:text-primary duration-200 transition-colors font-semibold"
+            >
+              Learn more
+              <ArrowRightCircle className={`!w-5 !h-5 `} />
+            </Link>
+          </div>
+        </div>
+        {/* will appear in md and larger screens only , the relative is to force the image to be inside the div*/}
+        <div className="relative md:block">
+          <Image
+            src={pizza}
+            alt="pizza logo"
+            fill
+            loading="eager"
+            priority
+            // to force the image to resrect it's width and height
+            // يعني هتظهر بالطول و العرض تبعها
+            // لو قلت الها cover => هتظهر بالطول و العرض يلي انا بخصصه الها
+            className="object-contain"
+          />
+        </div>
+      </div>
+    </section>
+  );
+};
+
+export default Hero;
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,9 @@
-import Image from "next/image";
+import Hero from "./-components/Hero";
 
 export default function Home() {
   return (
     <main>
-      <h1 className="text-3xl  text-primary">Hello World</h1>
+      <Hero />
     </main>
   );
 }
