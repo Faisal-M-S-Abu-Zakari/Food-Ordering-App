7d8a7975b34e5a3021f7b14a9239e7b9032e1427
Faisal <zkryfysl@gmail.com>
2025-06-08 09:39:47 +0300

    6- you will face error in image , so you have to detect your protocol and hostname that allowed to store images from them , in my case i allowed the http protocol and any hostname

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,10 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
+  // this is mean that my images will come from any https protocol with any hostname
+  images: {
+    remotePatterns: [{ protocol: "https", hostname: "**" }],
+  },
 };
 
 export default nextConfig;
